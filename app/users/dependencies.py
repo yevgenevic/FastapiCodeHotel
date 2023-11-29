15bde3fe2ec3349f28cce313a85a1053e46a889e
yevgenevic <yevgenevicalisher@gmail.com>
2023-11-29 18:27:51 +0500

    bug fix

---
+++ app/users/dependencies.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from fastapi import Request, HTTPException, Depends, status
+from jose import JWTError, jwt
+
+from app.users.dao import UsersDAO
+
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(
+            token, "alswdwqd", "HS256"
+        )
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    expire: str = payload.get("exp")
+    if (not expire) or (int(expire) < datetime.utcnow()):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user = await UsersDAO.find_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return user
